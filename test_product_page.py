d5d68d68b499bd19d97915104b695c4aae6bfe0a
lst <lessprit@mail.ru>
2026-03-04 22:09:41 +0500

    Добавлена параметризация в тест для поиска бага на промо страницах

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,20 @@
-import time
+import pytest
 
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+@pytest.mark.parametrize('promo', ["/?promo=offer0",
+                                   "/?promo=offer1",
+                                   "/?promo=offer2",
+                                   "/?promo=offer3",
+                                   "/?promo=offer4",
+                                   "/?promo=offer5",
+                                   "/?promo=offer6",
+                                   "/?promo=offer7",
+                                   "/?promo=offer8",
+                                   "/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, promo):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{promo}'
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
